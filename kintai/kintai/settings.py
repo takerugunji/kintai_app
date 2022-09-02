525d52fd25ed1e2503526e9668c6cb55fd75660c
takerugunji <takeru.gunji@gmail.com>
2022-09-02 21:42:55 +0900

    settings position change

--- kintai/kintai/settings.py
+++ kintai/kintai/settings.py
@@ -14,10 +14,8 @@ from pathlib import Path
 import os, django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# default
-#BASE_DIR = Path(__file__).resolve().parent.parent
 '''settingsの移動に伴う構成変更'''
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
--- kintai/kintai/settings/__init__.py
+++
@@ -1,5 +0,0 @@
-#from .local import *
-try:
-    from .base import *
-except:
-    pass
