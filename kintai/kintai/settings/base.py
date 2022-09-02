0faf6e1984ee6761a4cba13697349aabbdedd400
takerugunji <takeru.gunji@gmail.com>
2022-09-02 23:04:28 +0900

    settings call mod3

--- kintai/kintai/settings/base.py
+++ kintai/kintai/settings/base.py
@@ -15,7 +15,7 @@ import os, django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 '''settingsの移動に伴う構成変更'''
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
