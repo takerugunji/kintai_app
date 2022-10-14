87a9dadac619be7e97ab23dc8229b5f3f8d0f5c7
takerugunji <takeru.gunji@gmail.com>
2022-10-14 21:35:44 +0900

    modify call setting

--- kintai/manage.py
+++ kintai/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ['DJANGO_SETTINGS_MODULE'])
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ['DJANGO_SETTINGS_MODULE'])
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kintai.settings.local') # 開発環境実行用
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
