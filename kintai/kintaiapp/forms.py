4fec0457cfbd1cc529583e7b620074f73849ea83
takerugunji <takeru.gunji@gmail.com>
2022-10-14 21:38:09 +0900

    unuse cmd comment out

--- kintai/kintaiapp/forms.py
+++ kintai/kintaiapp/forms.py
@@ -4,6 +4,10 @@ from .models import SubmitAttendance # models.pyからclassを読み込む
 # 最初のページで出勤場所と出退勤打刻を表示させる
 class SubmitAttendanceForm(forms.ModelForm):
 
+    #def __init__(self, *args, **kwargs):
+    #    super().__init__(*args, **kwargs)
+    #    self.fields['in_out'].widget.attrs['disabled'] = 'disabled'
+
     class Meta:
         model = SubmitAttendance
         fields = ('place', 'in_out')
